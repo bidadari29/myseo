7efd2d1e42b99088a6426cb5230f7f34f3706eb0
bidadari29 <masterbidadari29@gmail.com>
2025-08-01 02:22:59 +0700

    Tambahkan Ahrefs JS snippet

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        {/* <script defer type="application/javascript" src="https://patches.ahrefs.com/9b00879b-d410-4840-99a1-9bebde3df8c9.js"></script> */}
+        <script
+          defer
+          type="application/javascript"
+          src="https://patches.ahrefs.com/9b00879b-d410-4840-99a1-9bebde3df8c9.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
