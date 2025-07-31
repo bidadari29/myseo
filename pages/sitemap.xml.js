78c63aa134a5833629d405f19ff01421151133c8
bidadari29 <masterbidadari29@gmail.com>
2025-07-31 22:48:54 +0700

    Add sitemap.xml route

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,21 +1,12 @@
 export async function getServerSideProps({ res }) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://tecno-educacion.com/</loc>
-  </url>
-  <url>
-    <loc>https://tecno-educacion.com/sitemap/page.xml</loc>
-  </url>
-  <url>
-    <loc>https://tecno-educacion.com/sitemap/promo.xml</loc>
-  </url>
-  <url>
-    <loc>https://tecno-educacion.com/sitemap/blog.xml</loc>
-  </url>
-</urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://tecno-educacion.com/</loc>
+    </url>
+  </urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
+  res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
 
